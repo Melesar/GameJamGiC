20b6cad213a14c3c19a8ed96a25480fdfd016268
Piotr Pietraszek <pietraszek.dev@gmail.com>
2021-10-20 18:54:42 +0200

    Tilt BuildTiles fixed

--- Assets/Scripts/ScriptsPP/BuildTileClick.cs
+++ Assets/Scripts/ScriptsPP/BuildTileClick.cs
@@ -7,7 +7,7 @@ namespace GameResources
         [SerializeField] private DiggingController _diggingController;
         [SerializeField] private GameObject[] CityPrefabArray;
         [SerializeField] private GameObject[] NaturePrefabArray;
-        [SerializeField] private GameObject Grid;
+        [SerializeField] private GameObject Ground;
 
        private Renderer _prevTile;
         private Vector3 _selectedTilePos;
@@ -21,38 +21,21 @@ namespace GameResources
         private void Update()
         {
 
-
-
             Ray screenRay = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(screenRay, out RaycastHit hit) == false) return;
 
-            var buildTile = hit.collider.GetComponentInParent<BuildPoints>();
-            
-            if (buildTile == null)
+            var buildTile = hit.collider.tag;
+
+            if (buildTile != "BoardTile")
             {
                 return;
             }
             
-            _selectedTilePos = buildTile.transform.position;
+            
             selectedTile = hit.collider.transform.gameObject;
-            Renderer TileRenderer;
-            TileRenderer = buildTile.GetComponent<Renderer>();
-            _initialColor = TileRenderer.material.color;
+            _selectedTilePos = selectedTile.transform.position;
 
-
-            if (!_prevTile)
-            {
-                _prevTile = TileRenderer;
-                TileRenderer.material.color = Color.white;
-            }
-
-            if (TileRenderer != _prevTile)
-            {
-                if (_prevTile.material.color == Color.white)
-                    _prevTile.material.color = _initialColor;
-                _prevTile = null;
-
-            }
+            TileColorChange();
 
             if (!Input.GetMouseButtonDown(0))
             {
@@ -76,8 +59,36 @@ namespace GameResources
             else
                 tile = Instantiate(NaturePrefabArray[(int)_selectedResource.Resource.Type]);
 
+            Quaternion rotation;
+            
             tile.transform.position = _selectedTilePos;
-            tile.transform.parent = Grid.transform;
+           
+            tile.transform.parent = Ground.transform;
+            rotation = Quaternion.Euler(0,0, Ground.transform.rotation.z);
+           tile.transform.rotation = selectedTile.transform.rotation;
+            Destroy(selectedTile);
+        }
+
+        void TileColorChange()
+        {
+            Renderer TileRenderer;
+            TileRenderer = selectedTile.GetComponent<Renderer>();
+            _initialColor = TileRenderer.material.color;
+
+
+            if (!_prevTile)
+            {
+                _prevTile = TileRenderer;
+                TileRenderer.material.color = Color.white;
+            }
+
+            if (TileRenderer != _prevTile)
+            {
+                if (_prevTile.material.color == Color.white)
+                    _prevTile.material.color = _initialColor;
+                _prevTile = null;
+
+            }
         }
     }
 }
